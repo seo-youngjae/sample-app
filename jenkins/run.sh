87b8ebce610db84937d7e6d7f3f013a836542262
seo youngjae <seoyoungjae707@gmail.com>
2025-10-02 03:40:18 +0900

    docker volume

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -5,14 +5,36 @@ IMAGE_NAME="custom-jenkins"
 VERSION="1.1"
 CPU_PLATFORM=${1:-amd64}
 
+# 컨테이너 실행 (도커 볼륨 사용)
 docker run -d \
   --name sample-jenkins \
   -p 8888:8080 \
   -p 50000:50000 \
-  -v $(pwd)/jenkins_home:/var/jenkins_home \
+  -v jenkins_home_data:/var/jenkins_home \
   -v $(pwd)/jenkins.yaml:/var/jenkins_config/jenkins.yaml \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -e CASC_JENKINS_CONFIG=/var/jenkins_config/jenkins.yaml \
   -e JAVA_OPTS="-Djenkins.install.runSetupWizard=false" \
   --privileged \
   ${IMAGE_NAME}-${CPU_PLATFORM}:${VERSION}
+
+
+
+# #!/bin/bash
+# set -eux
+
+# IMAGE_NAME="custom-jenkins"
+# VERSION="1.1"
+# CPU_PLATFORM=${1:-amd64}
+
+# docker run -d \
+#   --name sample-jenkins \
+#   -p 8888:8080 \
+#   -p 50000:50000 \
+#   -v $(pwd)/jenkins_home:/var/jenkins_home \
+#   -v $(pwd)/jenkins.yaml:/var/jenkins_config/jenkins.yaml \
+#   -v /var/run/docker.sock:/var/run/docker.sock \
+#   -e CASC_JENKINS_CONFIG=/var/jenkins_config/jenkins.yaml \
+#   -e JAVA_OPTS="-Djenkins.install.runSetupWizard=false" \
+#   --privileged \
+#   ${IMAGE_NAME}-${CPU_PLATFORM}:${VERSION}
